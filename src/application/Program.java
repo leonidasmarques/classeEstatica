976bceee1e99b485ab42deef30afaa198516b33f
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-21 20:23:23 -0300

    finalizando classe statica

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Scanner;
 
 public class Program {
+	
+	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -16,15 +18,21 @@ public class Program {
 		
 		double v = volume(raio);
 		
-		System.out.print("Circuferencia: %.2f%n ", c);
-		
-		
-		
-		
-		
-		
-		sc.close();
+		System.out.printf("Circuferencia: %.2f%n ", c);
+		System.out.printf("volume: %.2f%n ", v);
+		System.out.printf("valor de P: %.2f%n ", PI);
 
+		sc.close();
 	}
+	
+	
+	public static double circuferencia(double raio) {
+		return 2.0 * PI * raio;
+	}
+	
+	public static double volume(double raio) {
+		return 4 * PI * raio * raio * raio / 3;
+	}
+	
 
 }
