b6cff1b4ae70912a84f730b7ac8f93ee79de0e00
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-21 20:29:28 -0300

    aplicando segunda forma

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,37 +2,32 @@ package application;
 
 import java.util.Scanner;
 
+import util.Calculador;
+
 public class Program {
-	
-	public static final double PI = 3.14159;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		Calculador calc = new Calculador();
 		
 		System.out.println("Informe o valor do raio");
 		double raio = sc.nextDouble();
 		
-		double c = circuferencia(raio);
+		double c = calc.circunferencia(raio);
 		
 		
-		double v = volume(raio);
+		double v = calc.volume(raio);
 		
 		System.out.printf("Circuferencia: %.2f%n ", c);
 		System.out.printf("volume: %.2f%n ", v);
-		System.out.printf("valor de P: %.2f%n ", PI);
+		System.out.printf("valor de P: %.2f%n ", calc.PI);
 
 		sc.close();
 	}
 	
 	
-	public static double circuferencia(double raio) {
-		return 2.0 * PI * raio;
-	}
 	
-	public static double volume(double raio) {
-		return 4 * PI * raio * raio * raio / 3;
-	}
 	
 
 }
