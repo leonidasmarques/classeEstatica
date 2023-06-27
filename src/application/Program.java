2f6ce3b58b946c357e550c4ca1efa511e61cee55
Leonidas Marques <leonidasmmarques@outlook.com>
2023-06-27 14:20:06 -0300

    finalizando classe statica

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,19 +9,18 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		Calculador calc = new Calculador();
-		
+			
 		System.out.println("Informe o valor do raio");
 		double raio = sc.nextDouble();
 		
-		double c = calc.circunferencia(raio);
+		double c = Calculador.circunferencia(raio);
 		
 		
-		double v = calc.volume(raio);
+		double v = Calculador.volume(raio);
 		
 		System.out.printf("Circuferencia: %.2f%n ", c);
 		System.out.printf("volume: %.2f%n ", v);
-		System.out.printf("valor de P: %.2f%n ", calc.PI);
+		System.out.printf("valor de P: %.2f%n ", Calculador.PI);
 		
 		
 		//Testando a chave ssh depois de formar o pc
